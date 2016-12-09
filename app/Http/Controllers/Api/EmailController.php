f087ce2e1ff4e55ecc9716ce59284ab1084152ca
Mathieu TUDISCO <mathieu.tudisco@neoxia.com>
2016-12-09 16:34:51 +0100

    Traitement des erreurs dans les mails

--- app/Http/Controllers/Api/EmailController.php
+++ app/Http/Controllers/Api/EmailController.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Requests\EmailRequest;
-
 use App\Http\Controllers\Controller;
-use App\Models\Company;
-use App\Models\User;
+use App\Http\Requests\EmailRequest;
 use Illuminate\Mail\Message;
 use Mail;
 
@@ -15,14 +12,24 @@ class EmailController extends Controller
     public function email(EmailRequest $request)
     {
         extract($request->only(['name', 'message', 'email']), EXTR_SKIP);
-        Mail::raw($message, function (Message $m) use ($name, $email) {
-            $m->from('contact@forum-am.fr', 'Site Forum AM');
-            $m->replyTo($email, $name);
-            $m->to(env('EMAIL_CONTACT', 'contact@forum-am.fr'))->subject('Nouveau message depuis le site !');
-        });
+        try {
+            Mail::raw($message, function (Message $m) use ($name, $email) {
+                $m->from('contact@forum-am.fr', 'Site Forum AM');
+                $m->replyTo($email, $name);
+                $m->to(env('EMAIL_CONTACT', 'contact@forum-am.fr'))->subject('Nouveau message depuis le site !');
+            });
+        } catch (\Exception $e) {
+            return response()->json([
+                'code'    => 500,
+                'errors' => 'Une erreur s\'est produite, 
+                    veuillez réessayer plus tard, ou en informer l\'adminsitrateur.',
+                'exception' => $e->getMessage()
+            ], 500);
+        }
+
 
         return response()->json([
-            'code'   => 200,
+            'code'    => 200,
             'message' => 'Votre message a bien été envoyé, nous le traiterons dès que possible.',
         ], 200);
     }
