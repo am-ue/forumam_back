efbfcd16c74bd5144f4c3cecd256281ba9bb23cb
Mathieu TUDISCO <mathieu.tudisco@neoxia.com>
2016-12-09 15:27:06 +0100

    Correction envoi de mail.

--- app/Http/Controllers/Api/EmailController.php
+++ app/Http/Controllers/Api/EmailController.php
@@ -14,11 +14,16 @@ class EmailController extends Controller
 {
     public function email(EmailRequest $request)
     {
-        extract($request->only(['name', 'message', 'email']));
+        extract($request->only(['name', 'message', 'email']), EXTR_SKIP);
         Mail::raw($message, function (Message $m) use ($name, $email) {
             $m->from('contact@forum-am.fr', 'Site Forum AM');
             $m->replyTo($email, $name);
             $m->to(env('EMAIL_CONTACT', 'contact@forum-am.fr'))->subject('Nouveau message depuis le site !');
         });
+
+        return response()->json([
+            'code'   => 200,
+            'message' => 'Votre message a bien été envoyé, nous le traiterons dès que possible.',
+        ], 200);
     }
 }
